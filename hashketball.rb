cd79c17b5e87438be1b758b3ace12a1541f8c07c
Claudine McGowan <claudine.mcgowan@gmail.com>
2019-06-10 10:10:53 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,4 +1,24 @@
-# Write your code here!
+def game_hash
+  game_hash = {
+    :home => {
+      :team_name => {}
+      :colors =>{}
+      :players => {[]}
+    }
+    :away =>{
+      :team_name => {}
+      :colors =>{}
+      :players => {[]}
+      
+    }
+    
+    
+    
+    
+  }
+  
+  
+end
 
 
 
