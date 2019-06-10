6456dcbb7cbbc829de6c7d716933e96c9f20bb56
Claudine McGowan <claudine.mcgowan@gmail.com>
2019-06-10 10:33:04 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,11 +1,12 @@
+# Write your code here!
+
 def game_hash
   {
     home: {
       team_name: "Brooklyn Nets",
       colors: ["Black", "White"],
-      players: [
-        {
-          player_name: "Alan Anderson",
+      players: {
+        "Alan Anderson" => {
           number: 0,
           shoe: 16,
           points: 22,
@@ -13,9 +14,9 @@ def game_hash
           assists: 12,
           steals: 3,
           blocks: 1,
-          slam_dunks: 1
-        }, {
-          player_name: "Reggie Evans",
+          slam_dunks: 1,
+        },
+        "Reggie Evans" => {
           number: 30,
           shoe: 14,
           points: 12,
@@ -23,9 +24,9 @@ def game_hash
           assists: 12,
           steals: 12,
           blocks: 12,
-          slam_dunks: 7
-        }, {
-          player_name: "Brook Lopez",
+          slam_dunks: 7,
+        },
+        "Brook Lopez" => {
           number: 11,
           shoe: 17,
           points: 17,
@@ -33,9 +34,9 @@ def game_hash
           assists: 10,
           steals: 3,
           blocks: 1,
-          slam_dunks: 15
-        }, {
-          player_name: "Mason Plumlee",
+          slam_dunks: 15,
+        },
+        "Mason Plumlee" => {
           number: 1,
           shoe: 19,
           points: 26,
@@ -43,9 +44,9 @@ def game_hash
           assists: 6,
           steals: 3,
           blocks: 8,
-          slam_dunks: 5
-        }, {
-          player_name: "Jason Terry",
+          slam_dunks: 5,
+        },
+        "Jason Terry" => {
           number: 31,
           shoe: 15,
           points: 19,
@@ -53,16 +54,15 @@ def game_hash
           assists: 2,
           steals: 4,
           blocks: 11,
-          slam_dunks: 1
-        }
-      ]
+          slam_dunks: 1,
+        },
+      },
     },
     away: {
       team_name: "Charlotte Hornets",
       colors: ["Turquoise", "Purple"],
-      players: [
-        {
-          player_name: "Jeff Adrien",
+      players: {
+        "Jeff Adrien" => {
           number: 4,
           shoe: 18,
           points: 10,
@@ -70,9 +70,9 @@ def game_hash
           assists: 1,
           steals: 2,
           blocks: 7,
-          slam_dunks: 2
-        }, {
-          player_name: "Bismak Biyombo",
+          slam_dunks: 2,
+        },
+        "Bismak Biyombo" => {
           number: 0,
           shoe: 16,
           points: 12,
@@ -80,9 +80,9 @@ def game_hash
           assists: 7,
           steals: 7,
           blocks: 15,
-          slam_dunks: 10
-        }, {
-          player_name: "DeSagna Diop",
+          slam_dunks: 10,
+        },
+        "DeSagna Diop" => {
           number: 2,
           shoe: 14,
           points: 24,
@@ -90,9 +90,9 @@ def game_hash
           assists: 12,
           steals: 4,
           blocks: 5,
-          slam_dunks: 5
-        }, {
-          player_name: "Ben Gordon",
+          slam_dunks: 5,
+        },
+        "Ben Gordon" => {
           number: 8,
           shoe: 15,
           points: 33,
@@ -100,9 +100,9 @@ def game_hash
           assists: 2,
           steals: 1,
           blocks: 1,
-          slam_dunks: 0
-        }, {
-          player_name: "Brendan Haywood",
+          slam_dunks: 0,
+        },
+        "Brendan Haywood" => {
           number: 33,
           shoe: 15,
           points: 6,
@@ -110,19 +110,19 @@ def game_hash
           assists: 12,
           steals: 22,
           blocks: 5,
-          slam_dunks: 12
-        }
-    ]
-    }
+          slam_dunks: 12,
+        },
+      },
+    },
   }
 end
 
-def home_team_name
-  game_hash[:home][:team_name]
-end
+# Build a method, num_points_scored that takes in an argument of a player's name and returns the number of points scored for that player.
+
+# Think about where in the hash you will find a player's :points. How can you iterate down into that level? Think about the return value of your method. Remember that .each returns the original collection that you are iterating over. How can you return the number of points for a particular player?
 
 def num_points_scored(name)
-  game_hash.each do |location, team_data|
+  game_hash().each do |location, team_data|
     team_data[:players].each do |player_name, player_stat|
       if name == player_name
         return player_stat[:points]
@@ -130,5 +130,3 @@ def num_points_scored(name)
     end
   end
 end
-
-
